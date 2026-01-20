7d50c32256c92127ecfe22710199a7b917be6ebf
Lee-Rudy <120602756+Lee-Rudy@users.noreply.github.com>
2026-01-21 00:42:37 +0400

    Modification du test pour désactiver Spring Security, car l'endpoint d'inscription doit être public

--- src/test/java/com/example/auth/inscriptionTest/adapters/in/InscriptionControllerTest.java
+++ src/test/java/com/example/auth/inscriptionTest/adapters/in/InscriptionControllerTest.java
@@ -5,6 +5,7 @@ import com.example.auth.inscription.entity.Users;
 import com.example.auth.inscription.ports.in.InscriptionUseCase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -15,7 +16,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(InscriptionController.class)
+@WebMvcTest(value = InscriptionController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 class InscriptionControllerTest {
 
     @Autowired
