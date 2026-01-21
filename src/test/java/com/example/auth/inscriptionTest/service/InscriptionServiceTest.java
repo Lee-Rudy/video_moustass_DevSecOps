a1f8b1d223f9cb8a93abf9e6cba43ccde894f4b9
Lee-Rudy <120602756+Lee-Rudy@users.noreply.github.com>
2026-01-21 16:58:42 +0400

    update : private static final String password = "PasswordA12";

--- src/test/java/com/example/auth/inscriptionTest/service/InscriptionServiceTest.java
+++ src/test/java/com/example/auth/inscriptionTest/service/InscriptionServiceTest.java
@@ -19,7 +19,7 @@ class InscriptionServiceTest {
     private BCryptPasswordEncoder encoder;
     private InscriptionService service;
 
-    private static final String password = "PasswordA1";
+    private static final String password = "PasswordA12";
 
     @BeforeEach
     void setUp() {
