1bfe50e03cb6f886b2c1792b0b9762a8415a9ed6
Lee-Rudy <120602756+Lee-Rudy@users.noreply.github.com>
2026-01-26 02:19:12 +0400

    correction sonarCloud

--- src/main/java/com/example/auth/login/controller/LoginController.java
+++ src/main/java/com/example/auth/login/controller/LoginController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.auth.login.models.LoginService;
-import com.example.auth.login.models.LoginService.LoginResponse;
+
 
 @RestController
 @RequestMapping("/api/login")
